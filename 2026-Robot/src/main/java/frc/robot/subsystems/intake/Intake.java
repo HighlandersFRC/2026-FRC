9e364fa5cf37d4b797d60a91a73eb8069bc989c9
Ben-Mick <benjaminmick@gmail.com>
2026-01-11 10:39:44 -0700

    intake structure

---
+++ 2026-Robot/src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  public Intake() {
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
