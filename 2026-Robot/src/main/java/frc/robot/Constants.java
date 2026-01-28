b327d98bee74c0356ff1c1a30846a241832c90d5
Ben-Mick <benjaminmick@gmail.com>
2026-01-28 08:41:51 -0700

    tuning the turret gear ratio hopefully this one is correct

--- 2026-Robot/src/main/java/frc/robot/Constants.java
+++ 2026-Robot/src/main/java/frc/robot/Constants.java
@@ -500,10 +500,13 @@ public final class Constants {
 
                 public static final class Shooter {
                         public static final double FLYWHEEL_GEAR_RATIO = 1.0 / 1.0;
-                        public static final double HOOD_GEAR_RATIO = 845.0 / 7.0;
-                        public static final double TURRET_GEAR_RATIO = 40.23809523809523
-                        //  * (166.0 / 181.0)
-                        ;
+                        public static final double HOOD_GEAR_RATIO = 201.0;
+                        // public static final double HOOD_GEAR_RATIO = 845.0 / 7.0; // old shooter
+                        public static final double TURRET_GEAR_RATIO = 37.723;
+                        // public static final double TURRET_GEAR_RATIO = 6812.0 / 180.0;
+                        // public static final double TURRET_GEAR_RATIO = 40.23809523809523;
+                        // public static final double TURRET_GEAR_RATIO = 43.112;
+                        // public static final double TURRET_GEAR_RATIO = 50.55;
                 }
 
                 public static final class Intake {
