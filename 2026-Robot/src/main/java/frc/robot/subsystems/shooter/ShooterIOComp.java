add6d519a9ed274472bc0569e355bd0d8bf29753
Ben-Mick <benjaminmick@gmail.com>
2026-02-13 20:38:09 -0700

    new encoder zeros

--- 2026-Robot/src/main/java/frc/robot/subsystems/shooter/ShooterIOComp.java
+++ 2026-Robot/src/main/java/frc/robot/subsystems/shooter/ShooterIOComp.java
@@ -161,14 +161,14 @@ class ShooterIOComp implements ShooterIO {
                 // CANcoder Configuration
                 CANcoderConfiguration encoderOneConfig = new CANcoderConfiguration();
                 encoderOneConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
-                encoderOneConfig.MagnetSensor.MagnetOffset = -0.061279296875; // TODO: Try calculating offset from
-                                                                              // previous zero
-                                                                              // data
+                encoderOneConfig.MagnetSensor.MagnetOffset = -0.765625; // TODO: Try calculating offset from
+                                                                        // previous zero
+                                                                        // data
                 encoderOneConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1.0;
                 encoderOne.getConfigurator().apply(encoderOneConfig);
                 CANcoderConfiguration encoderTwoConfig = new CANcoderConfiguration();
                 encoderTwoConfig.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
-                encoderTwoConfig.MagnetSensor.MagnetOffset = -0.602294921875;
+                encoderTwoConfig.MagnetSensor.MagnetOffset = -0.30322265625;
                 encoderTwoConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1.0;
                 encoderTwo.getConfigurator().apply(encoderTwoConfig);
 
