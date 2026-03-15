ffd9badd9ecaa58e3c4e7597fd7ae3160e0310ab
Ben-Mick <benjaminmick@gmail.com>
2026-03-15 17:50:32 -0600

    tuning limelight yaw

--- 2026-Robot/src/main/java/frc/robot/Constants.java
+++ 2026-Robot/src/main/java/frc/robot/Constants.java
@@ -680,7 +680,7 @@ public final class Constants {
                 public static final Rotation3d LIMELIGHT_ROTATION_RELATIVE_TO_TURRET = new Rotation3d(
                                 Math.toRadians(0.0),
                                 Math.toRadians(-26.0),
-                                Math.toRadians(0.0));
+                                Math.toRadians(3.5));
 
                 public static final Transform3d turretToLimelight = new Transform3d(LIMELIGHT_TO_TURRET_OFFSET,
                                 LIMELIGHT_ROTATION_RELATIVE_TO_TURRET);
