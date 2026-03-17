dcb740b267dd243c71c03730b53181229c40312e
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2026-03-16 21:27:06 -0600

    new bump offset

--- 2026-Robot/src/main/java/frc/robot/Constants.java
+++ 2026-Robot/src/main/java/frc/robot/Constants.java
@@ -265,9 +265,8 @@ public final class Constants {
         }
 
         public static final class Field {
-                public static final double BUMP_LENGTH = 1.156 / 2.0; // 1.12776 actual
                 public static final double NEUTRAL_ZONE_BUMP_X_POSITION_BLUE = 5.37;
-                public static final double ALLIANCE_ZONE_BUMP_X_POSITION_BLUE = 5.37;
+                public static final double ALLIANCE_ZONE_BUMP_X_POSITION_BLUE = 3.67;
                 public static final double NEUTRAL_ZONE_BUMP_X_POSITION_RED = Constants.Physical.FIELD_LENGTH
                                 - NEUTRAL_ZONE_BUMP_X_POSITION_BLUE;
                 public static final double ALLIANCE_ZONE_BUMP_X_POSITION_RED = Constants.Physical.FIELD_LENGTH
