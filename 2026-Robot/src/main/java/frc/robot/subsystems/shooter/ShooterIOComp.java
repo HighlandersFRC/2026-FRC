5c3b9bd23beaf6bb055c00eb7e280bc4f1a861e0
Rishabh Kapoor <rishabhkapoor@live.com>
2026-04-30 17:42:19 -0600

    rezeroed turret

--- 2026-Robot/src/main/java/frc/robot/subsystems/shooter/ShooterIOComp.java
+++ 2026-Robot/src/main/java/frc/robot/subsystems/shooter/ShooterIOComp.java
@@ -262,14 +262,14 @@ class ShooterIOComp implements ShooterIO {
                 // CANcoder Configuration
                 CANcoderConfiguration encoderOneConfig = new CANcoderConfiguration();
                 encoderOneConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
-                encoderOneConfig.MagnetSensor.MagnetOffset = -0.8828125; // TODO: Try calculating offset from
-                                                                         // previous zero
-                                                                         // data
+                encoderOneConfig.MagnetSensor.MagnetOffset = -0.15380859375; // TODO: Try calculating offset from
+                                                                             // previous zero
+                                                                             // data
                 encoderOneConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1.0;
                 encoderOne.getConfigurator().apply(encoderOneConfig);
                 CANcoderConfiguration encoderTwoConfig = new CANcoderConfiguration();
                 encoderTwoConfig.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
-                encoderTwoConfig.MagnetSensor.MagnetOffset = -0.226318359375;
+                encoderTwoConfig.MagnetSensor.MagnetOffset = -0.3623046875;
                 encoderTwoConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1.0;
                 encoderTwo.getConfigurator().apply(encoderTwoConfig);
 
